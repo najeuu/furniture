8dd6727dfa3dd5d8f7a0123d6b6f05322bfc4a72
wawawonka <najeu05@gmail.com>
2024-02-29 13:42:12 +0700

    Update composer lock dan penambahan file baruHomeController.php contact.blade.php home.blade.php dan web.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <!-- It is quality rather than quantity that matters. - Lucius Annaeus Seneca -->
+    Selamat datang di home {{$nama}}<br>
+    Anda adalah seorang {{$pekerjaan}}
+    
+</div>
